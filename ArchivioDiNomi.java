24669d5205d629e46185f8fb649185482364e304
Diego D'Ortenzio <160123629+DOrtenzio@users.noreply.github.com>
2024-03-14 11:56:57 +0100

    Versione con funzioni 1-6-0

--- ArchivioDiNomi.java
+++ ArchivioDiNomi.java
@@ -2,6 +2,8 @@ import java.util.Scanner;
 public class ArchivioDiNomi{
   public static void main (String[]args){
     Scanner in = new Scanner (System.in);
+    int conta=0;
+    String [] parole=new String[100];
     do{
 	System.out.println ("Scegli una tra le opzioni proposte:");
 	System.out.println ("1 - Aggiunta di un nome;");
@@ -16,19 +18,41 @@ public class ArchivioDiNomi{
 	int scelta = in.nextInt ();
 	switch (scelta){
 	        case 1:
+	                conta++;
+	                System.out.println("Inserire la parola da inserire:");
+	                String p=in.nextLine();
+	                parole=isAggiunta(parole,p,conta);
+	                break;
                 case 2:
                 case 3:
                 case 4:
                 case 5:
                 case 6:
+                        if (conta==0)
+                                System.out.println("Inserire almeno un valore prima");
+                        else{
+                                for (int i=0;i<conta;i++){
+                                        System.out.printl(parole,i)
+                                }
+                        }
+                        break;
                 case 7:
                 case 8:
                 case 9:
+                case 0:
                         System.out.println("Ciao");
                         break;
                 default:
                         System.out.println("Inserire uno dei valori specificati");
 	}
       }while (scelta!=0);
+    }
+    private static String [] isAggiunta (String [] parole, String p, int conta){
+            parole[conta-1]=p;
+            return parole;
+    }
+    private static String isStampa (String [] parole, int i){
+            String s=parole[i];
+            return s;
     }
     }
