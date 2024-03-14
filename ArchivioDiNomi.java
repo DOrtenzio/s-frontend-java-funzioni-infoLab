e6ba752a9c412d79b7051977784a2ff62f8025a8
Diego D'Ortenzio <160123629+DOrtenzio@users.noreply.github.com>
2024-03-14 17:07:11 +0100

    Versione 1-6-2

--- ArchivioDiNomi.java
+++ ArchivioDiNomi.java
@@ -1,68 +1,89 @@
 import java.util.Scanner;
 public class ArchivioDiNomi{
-  public static void main (String[]args){
-    Scanner in = new Scanner (System.in);
-    int conta=0;
-    String [] parole=new String[100];
-    do{
-	System.out.println ("Scegli una tra le opzioni proposte:");
-	System.out.println ("1 - Aggiunta di un nome;");
-	System.out.println ("2 - Cancellazione del singolo nome;");
-	System.out.println ("3 - Ricerca sequenziale di un nome;");
-	System.out.println ("4 - Visualizza nomi ripetuti con numero ripetizioni;");
-	System.out.println ("5 - Modifica di un nome;");
-	System.out.println ("6 - Visualizzazione di tutti i nomi presenti;");
-	System.out.println ("7 - Ricerca del nome piC9 lungo e piC9 corto;");
-	System.out.println ("8 - Cancellazione di tutte le occorrenze di un nome;");
-	System.out.println ("0 - Uscita ");
-	int scelta = in.nextInt();
-	switch (scelta){
-	        case 1:
-	                conta++;
-	                System.out.println("Inserire la parola da inserire:");
-	                String p=in.nextLine();
-	                parole=isAggiunta(parole,p,conta);
-	                break;
+    public static void main (String[]args){
+        Scanner in = new Scanner (System.in);
+        int conta=0;
+        boolean ce=false;
+        int scelta,posizione = 0;
+        String [] parole=new String[100];
+        do{
+            System.out.println ("Scegli una tra le opzioni proposte:");
+            System.out.println ("1 - Aggiunta di un nome;");
+            System.out.println ("2 - Cancellazione del singolo nome;");
+            System.out.println ("3 - Ricerca sequenziale di un nome;");
+            System.out.println ("4 - Visualizza nomi ripetuti con numero ripetizioni;");
+            System.out.println ("5 - Modifica di un nome;");
+            System.out.println ("6 - Visualizzazione di tutti i nomi presenti;");
+            System.out.println ("7 - Ricerca del nome piC9 lungo e piC9 corto;");
+            System.out.println ("8 - Cancellazione di tutte le occorrenze di un nome;");
+            System.out.println ("0 - Uscita ");
+            scelta = in.nextInt();
+            in.nextLine();
+            switch (scelta){
+                case 1:
+                    conta++;
+                    System.out.println("Inserire la parola da inserire:");
+                    String p=in.nextLine();
+                    parole=isAggiunta(parole,p,conta);
+                    break;
                 case 2:
-                        if (conta==0)
-                                System.out.println("Inserire almeno un valore prima");
-                        else{
-                                System.out.println("Inserire la stringa da ricercare");
-                                String sricerca=in.nextLine();
-                                for (int i=0;i<conta;i++){
-                                        
-                                }
+                    if (conta==0)
+                        System.out.println("Inserire almeno un valore prima");
+                    else{
+                        System.out.println("Inserire la stringa da ricercare");
+                        String sricerca=in.nextLine();
+                        ce=false;
+                        for (int i=0;i<conta;i++){
+                            if (sricerca.equals(parole[i])){
+                                ce=true;
+                                posizione=i;
+                                break;
+                            }
                         }
-                        break;
+                        if (ce){
+                            conta--;
+                            parole=isCancellazioneSingola(parole,conta,posizione);
+                        }
+                        else
+                            System.out.println("Non abbiamo ritrovato il termine "+sricerca+" nella lista.");
+                    }
+                    break;
                 case 3:
                 case 4:
                 case 5:
                 case 6:
-                        if (conta==0)
-                                System.out.println("Inserire almeno un valore prima");
-                        else{
-                                for (int i=0;i<conta;i++){
-                                        System.out.printl(parole,i);
-                                }
+                    if (conta==0)
+                        System.out.println("Inserire almeno un valore prima");
+                    else{
+                        for (int i=0;i<conta;i++){
+                            System.out.print(isStampa(parole,i)+" ");
                         }
-                        break;
+                        System.out.println();
+                    }
+                    break;
                 case 7:
                 case 8:
                 case 9:
                 case 0:
-                        System.out.println("Ciao");
-                        break;
+                    System.out.println("Ciao");
+                    break;
                 default:
-                        System.out.println("Inserire uno dei valori specificati");
-	}
-      }while (scelta!=0);
+                    System.out.println("Inserire uno dei valori specificati");
+            }
+        }while (scelta!=0);
     }
     private static String [] isAggiunta (String [] parole, String p, int conta){
-            parole[conta-1]=p;
-            return parole;
+        parole[conta-1]=p;
+        return parole;
     }
     private static String isStampa (String [] parole, int i){
-            String s=parole[i];
-            return s;
+        String s=parole[i];
+        return s;
     }
+    private static String [] isCancellazioneSingola (String [] parole, int conta, int posizione){
+        for (int i=posizione;i<conta;i++){
+          parole[i]=parole[i+1];
+        }
+        return parole;
     }
+}
